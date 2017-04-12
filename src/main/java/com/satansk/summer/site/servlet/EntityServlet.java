3d94d5500b0ffa859b05276f86baa5c7a7aec183
satansk <satansk@hotmail.com>
2017-04-13 00:08:56 +0800

    通过 id 定位查询结果

--- src/main/java/com/satansk/summer/site/servlet/EntityServlet.java
+++ src/main/java/com/satansk/summer/site/servlet/EntityServlet.java
@@ -63,6 +63,14 @@ public class EntityServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String id = req.getParameter("id");
+		
+		if (id == null) {
+			resp.getWriter().write("id can't be empty!");
+			return;
+		}
+		
 		EntityManager manager = null;
 		EntityTransaction transaction = null;
 		
@@ -91,7 +99,7 @@ public class EntityServlet extends HttpServlet {
 			transaction.commit();
 			
 			// 响应请求
-			resp.getWriter().write(publishers.get(0).toString());
+			resp.getWriter().write(publishers.get(Integer.valueOf(id)).toString());
 			
 		} catch(Exception e) {
 			
