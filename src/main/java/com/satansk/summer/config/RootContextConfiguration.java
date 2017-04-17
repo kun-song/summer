b475361233b7a47fa6e9b81ba797ecaaa4f1dcaa
satansk <satansk@hotmail.com>
2017-04-17 08:54:14 +0800

    使用 Java 配置持久化单元、事务管理

--- src/main/java/com/satansk/summer/config/RootContextConfiguration.java
+++ src/main/java/com/satansk/summer/config/RootContextConfiguration.java
@@ -1,9 +1,26 @@
 package com.satansk.summer.config;
 
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.persistence.SharedCacheMode;
+import javax.persistence.ValidationMode;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -11,11 +28,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Configuration
+@EnableTransactionManagement(
+        mode = AdviceMode.PROXY, proxyTargetClass = false,
+        order = Ordered.LOWEST_PRECEDENCE
+)
 @ComponentScan(
 		basePackages = "com.satansk.summer.site",
 		excludeFilters = @ComponentScan.Filter({ Controller.class, ControllerAdvice.class })
 		)
-public class RootContextConfiguration {
+public class RootContextConfiguration implements TransactionManagementConfigurer {
     @Bean
     public ObjectMapper objectMapper()
     {
@@ -26,4 +47,60 @@ public class RootContextConfiguration {
                 false);
         return mapper;
     }
+    
+    /**
+     * 在根应用上下文中数据源，需要在 Tomcat 的 context.xml 文件中预先定义好数据源
+     * 
+     * @return 返回查找到的数据源
+     */
+    @Bean
+    public DataSource springJpaDataSource() {
+    	JndiDataSourceLookup lookup = new JndiDataSourceLookup();
+    	return lookup.getDataSource("jdbc/EntityMappings");
+    }
+    
+    /**
+     * 创建持久化单元，代替原来 src/main/source/persistence.xml 配置文件
+     * 
+     * @return 创建好的持久化单元
+     */
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+    	Map<String, Object> properties = new Hashtable<>();
+    	properties.put("javax.persistence.schema-generation.database.action", "none");
+    	
+    	HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+    	adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
+    	
+    	LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+    	factory.setJpaVendorAdapter(adapter);
+    	// 上面获取的 DataSource
+    	factory.setDataSource(this.springJpaDataSource());
+    	factory.setPackagesToScan("com.satansk.summer.site.entity");
+    	factory.setSharedCacheMode(SharedCacheMode.ENABLE_SELECTIVE);
+    	factory.setValidationMode(ValidationMode.NONE);
+    	factory.setJpaPropertyMap(properties);
+    	
+    	return factory;
+    }
+    
+    /**
+     * 使用 @EnableTransactionManagement 注解必须提供一个 PlatformTransactionManager 的默认实现
+     * 
+     * @return 构造好的 JpaTransactionManager
+     */
+    @Bean
+    public PlatformTransactionManager jpaTransactionManager() {
+    	return new JpaTransactionManager(this.entityManagerFactoryBean().getObject());
+    }
+
+    /**
+     * 1. 事务管理默认返回第一个找到的 PlatformTransactionManager，但上下文可以存在多个 PlatformTransactionManager。
+     * 2. Spring 总是使用 TransactionManagementConfigurer 接口的 annotationDrivenTransactionManager 方法返回的
+     * 		管理器作为 @Transactional 方法的默认管理器。
+     */
+	@Override
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		return this.jpaTransactionManager();
+	}
 }
