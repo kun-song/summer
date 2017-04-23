474087accd2b1803b049810eececa9196bfabaee
satansk <satansk@hotmail.com>
2017-04-23 22:57:35 +0800

    Class<?> 替代 Class

--- src/main/java/com/satansk/summer/site/repository/GenericBaseRepository.java
+++ src/main/java/com/satansk/summer/site/repository/GenericBaseRepository.java
@@ -24,7 +24,7 @@ public abstract class GenericBaseRepository<I extends Serializable, E extends Se
 				throw new IllegalStateException();
 			}
 			
-			genericSuperclass = ((Class)genericSuperclass).getGenericSuperclass();
+			genericSuperclass = ((Class<?>)genericSuperclass).getGenericSuperclass();
 		}
 		
 		ParameterizedType type = (ParameterizedType) genericSuperclass;
