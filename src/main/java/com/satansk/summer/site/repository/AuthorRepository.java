a6d9472e12a648aa1aca73c1b33e9a3eead3d4a1
satansk <satansk@hotmail.com>
2017-05-05 08:57:28 +0800

    删除冗余代码

--- src/main/java/com/satansk/summer/site/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.satansk.summer.site.repository;
-
-import com.satansk.summer.site.entity.Author;
-
-public interface AuthorRepository extends GenericRepository<Long, Author> {
-
-}
--- src/main/java/com/satansk/summer/site/repository/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.satansk.summer.site.repository;
-
-import com.satansk.summer.site.entity.Publisher;
-
-public interface PublisherRepository extends GenericRepository<Long, Publisher> {
-	
-}
