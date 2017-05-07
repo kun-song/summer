59549e76cb57a471b12388a9944a50134b3e3364
satansk <satansk@hotmail.com>
2017-05-07 11:28:44 +0800

    去除自定义的 GenericRepository

--- src/main/java/com/satansk/summer/site/repository/impl/DefaultBookRepository.java
+++ src/main/java/com/satansk/summer/site/repository/impl/DefaultBookRepository.java
@@ -1,5 +1,7 @@
 package com.satansk.summer.site.repository.impl;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -8,19 +10,21 @@ import org.springframework.stereotype.Repository;
 
 import com.satansk.summer.site.entity.Book;
 import com.satansk.summer.site.repository.BookRepository;
-import com.satansk.summer.site.repository.GenericJpaRepository;
 
 /**
  * 1. 大部分方法在 GenericJpaRepository 中已经实现，只需要实现 BookRepository 中单独定义的 getByIsbn 即可。
  */
 @Repository
-public class DefaultBookRepository extends GenericJpaRepository<Long, Book> implements BookRepository {
+public class DefaultBookRepository implements BookRepository {
 
+	@Inject
+	private EntityManager entityManager;
+	
 	@Override
 	public Book getByIsbn(String isbn) {
 		CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
-		CriteriaQuery<Book> query = builder.createQuery(this.entityClass);
-		Root<Book> root = query.from(this.entityClass);
+		CriteriaQuery<Book> query = builder.createQuery(Book.class);
+		Root<Book> root = query.from(Book.class);
 		
 		return this.entityManager
 				.createQuery(query.select(root).where(builder.equal(root.get("isbn"), isbn)))
