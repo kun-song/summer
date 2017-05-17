35b99e393a2d112d317d441265aad3166d428b71
satansk <satansk@hotmail.com>
2017-05-17 21:04:40 +0800

    抽离 AppComponent 中 html 文件

--- src/main/angular-app/src/app/app.component.ts
+++ src/main/angular-app/src/app/app.component.ts
@@ -5,19 +5,7 @@ import { ApiSpecService } from './shared/api-spec.service';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <header [title] = appTitle></header>
-    <h2>ApiSpec List</h2>
-    <ul class = "apispec-list">
-      <li *ngFor = "let apispec of apiSpecList"
-          [class.selected] = "apispec === apiSpecSelected"
-          (click) = "onSelect(apispec)">
-        <span class = "badge">{{ apispec.id }}</span> {{ apispec.title }}
-      </li>
-    </ul>
-
-    <api-spec-detail [apiSpec] = "apiSpecSelected"></api-spec-detail>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [ ApiSpecService ]
 })
