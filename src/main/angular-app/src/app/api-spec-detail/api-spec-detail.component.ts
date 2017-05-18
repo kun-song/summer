e28f5269b2ebbb666a487cfed810c340f0a874de
satansk <satansk@hotmail.com>
2017-05-18 08:39:59 +0800

    将 ApiSpecDetail 组件的 HTML 抽离到单独的文件中

--- src/main/angular-app/src/app/api-spec-detail/api-spec-detail.component.ts
+++ src/main/angular-app/src/app/api-spec-detail/api-spec-detail.component.ts
@@ -4,16 +4,7 @@ import { ApiSpec } from "../mock/api-spec";
 
 @Component({
   selector: 'api-spec-detail',
-  template: `
-    <div *ngIf = "apiSpec">
-      <h2>{{ apiSpec.id }} details!</h2>
-      <div><label>id: </label>{{ apiSpec.id }}</div>
-      <div>
-        <label>title: </label>
-        <input [(ngModel)] = "apiSpec.title" placeholder="title"/>
-      </div>
-    </div>
-  `
+  templateUrl: './api-spec-detail.component.html', 
 })
 export class ApiSpecDetailComponent {
   @Input() apiSpec: ApiSpec;
