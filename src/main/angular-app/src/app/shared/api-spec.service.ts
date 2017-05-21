bc4f201689a78cc1d1d35f24f3f4391484fbf812
satansk <satansk@hotmail.com>
2017-05-21 19:50:05 +0800

    http 初步配置

--- src/main/angular-app/src/app/shared/api-spec.service.ts
+++ src/main/angular-app/src/app/shared/api-spec.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { ApiSpec } from '../mock/api-spec';
 import { APISPECLIST } from '../mock/api-spec-mock';
 
 @Injectable()
 export class ApiSpecService {
+
+  private apiSpecListUrl = 'http://127.0.0.1:8080/rest/api/spec';
+
+  constructor(private http: Http) {}
+
+
   getApiSpecList(): Promise<ApiSpec[]> {
-    return Promise.resolve(APISPECLIST);
+    return this.http.get(this.apiSpecListUrl)
+                .toPromise()
+                .then(response => response.json().data as ApiSpec[])
+                .catch(this.errorHandler);
   }
 
   getApiSpec(id: number) {
-    return this.getApiSpecList()
-                .then(list => list.find(apiSpec => apiSpec.id === id));
+    const url = `${this.apiSpecListUrl}/${id}`;
+    return this.http.get(url)
+                .toPromise()
+                .then(response => response.json().data as ApiSpec)
+                .catch(this.errorHandler);
+  }
+
+  // 内部异常处理
+  private errorHandler(error: any): Promise<any> {
+    console.log('An error occured: ', error);
+    return Promise.reject(error.message || error);
   }
 }
