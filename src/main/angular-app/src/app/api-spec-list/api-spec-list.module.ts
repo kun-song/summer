7e7ebf9a86528fd262453d42aabaf9769d86aeee
satansk <satansk@hotmail.com>
2017-05-22 23:36:55 +0800

    修复启动错误

--- src/main/angular-app/src/app/api-spec-list/api-spec-list.module.ts
+++ src/main/angular-app/src/app/api-spec-list/api-spec-list.module.ts
@@ -1,8 +1,6 @@
 import { NgModule }         from '@angular/core';
 import { BrowserModule }    from '@angular/platform-browser';
 
-import { ApiSpecListComponent }  from './api-spec-list.component';
-
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ ApiSpecListComponent ],
--- src/main/angular-app/src/app/app.component.ts
+++ src/main/angular-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { ApiSpec } from './mock/api-spec';
 import { ApiSpecService } from './shared/api-spec.service';
@@ -8,6 +8,6 @@ import { ApiSpecService } from './shared/api-spec.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   appTitle = 'Elastic API Manager';
 }
