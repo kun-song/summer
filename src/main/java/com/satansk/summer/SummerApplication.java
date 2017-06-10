4101339a9b45311e783559d7c462648085211136
satansk <satansk@hotmail.com>
2017-06-10 16:58:27 +0800

    过滤器迁移完成

--- src/main/java/com/satansk/summer/SummerApplication.java
+++ src/main/java/com/satansk/summer/SummerApplication.java
@@ -2,8 +2,10 @@ package com.satansk.summer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SummerApplication {
 
 	public static void main(String[] args) {
