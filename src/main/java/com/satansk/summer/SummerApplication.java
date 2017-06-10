d8508ded60505986abd26f393f0cbf6cb38c3bac
satansk <satansk@hotmail.com>
2017-06-10 16:06:45 +0800

    生成 SpringBoot 架子

---
+++ src/main/java/com/satansk/summer/SummerApplication.java
@@ -0,0 +1,12 @@
+package com.satansk.summer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerApplication.class, args);
+	}
+}
